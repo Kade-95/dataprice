02129d6acc9cb93b3af4238bebb520fe2922237a
kennedy.ikeka <kennedy.ikeka@ipigroupng.com>
2020-08-20 20:31:07 +0100

    First Commit

---
+++ data.js
@@ -0,0 +1,173 @@
+let data = [
+    {
+        "City/Country": "Dublin, Germany",
+        "Avg Speed(Mbps)": "300/-",
+        "Avg Cost": "$158",
+        "DUration": "-",
+        "ISP": " "
+    },
+    {
+        "City/Country": "Poznan, Poland",
+        "Avg Speed(Mbps)": "-/-",
+        "Avg Cost": "100zł (~30USD)",
+        "DUration": "-",
+        "ISP": "-"
+    },
+    {
+        "City/Country": "India",
+        "Avg Speed(Mbps)": "48/-",
+        "Avg Cost": "966Rs",
+        "DUration": "-",
+        "ISP": "Hathway Broadband"
+    },
+    {
+        "City/Country": "Berlin, Germany",
+        "Avg Speed(Mbps)": "100/-",
+        "Avg Cost": "40€",
+        "DUration": "-",
+        "ISP": "Vodafone"
+    },
+    {
+        "City/Country": "Berlin, Germany",
+        "Avg Speed(Mbps)": "200/-",
+        "Avg Cost": "31€",
+        "DUration": "-",
+        "ISP": "Pyur"
+    },
+    {
+        "City/Country": "Italy",
+        "Avg Speed(Mbps)": "500/200",
+        "Avg Cost": "30€",
+        "DUration": "-",
+        "ISP": "-"
+    },
+    {
+        "City/Country": "Portugal",
+        "Avg Speed(Mbps)": "400/100",
+        "Avg Cost": "36€",
+        "DUration": "-",
+        "ISP": "-"
+    },
+    {
+        "City/Country": "Slovenia",
+        "Avg Speed(Mbps)": "100/10",
+        "Avg Cost": "24€",
+        "DUration": "-",
+        "ISP": "-"
+    },
+    {
+        "City/Country": "Ireland",
+        "Avg Speed(Mbps)": "500/-",
+        "Avg Cost": "60€",
+        "DUration": "-",
+        "ISP": "Virgin"
+    },
+    {
+        "City/Country": "Budapest, Hungary",
+        "Avg Speed(Mbps)": "300/-",
+        "Avg Cost": "$10",
+        "DUration": "-",
+        "ISP": "-"
+    },
+    {
+        "City/Country": "Lusaka, Zambia",
+        "Avg Speed(Mbps)": "3/-",
+        "Avg Cost": "ZMW 349(19)",
+        "DUration": "-",
+        "ISP": "Zamtel"
+    },
+    {
+        "City/Country": "Morroco",
+        "Avg Speed(Mbps)": "4/-",
+        "Avg Cost": "DHS 200 ($20)",
+        "DUration": "-",
+        "ISP": "IAM"
+    },
+    {
+        "City/Country": "Hong Kong",
+        "Avg Speed(Mbps)": "1024/-",
+        "Avg Cost": "$20",
+        "DUration": "-",
+        "ISP": "-"
+    },
+    {
+        "City/Country": "New York",
+        "Avg Speed(Mbps)": "210/11",
+        "Avg Cost": "$70",
+        "DUration": "-",
+        "ISP": "Spectrum"
+    },
+    {
+        "City/Country": "Nigeria",
+        "Avg Speed(Mbps)": "30/-",
+        "Avg Cost": "$3",
+        "DUration": "-",
+        "ISP": "-"
+    },
+    {
+        "City/Country": "Salt Lake City, Utah",
+        "Avg Speed(Mbps)": "780/890",
+        "Avg Cost": "$65",
+        "DUration": "1 month",
+        "ISP": "-"
+    },
+    {
+        "City/Country": "Netherlands",
+        "Avg Speed(Mbps)": "1024/-",
+        "Avg Cost": "$46",
+        "DUration": "-",
+        "ISP": "-"
+    },
+    {
+        "City/Country": "Portugal",
+        "Avg Speed(Mbps)": "500/-",
+        "Avg Cost": "35€ ($42)",
+        "DUration": "-",
+        "ISP": "-"
+    },
+    {
+        "City/Country": "United Kingdom(UK)",
+        "Avg Speed(Mbps)": "1024/1024",
+        "Avg Cost": "$40",
+        "DUration": "-",
+        "ISP": "-"
+    },
+    {
+        "City/Country": "Venezuela",
+        "Avg Speed(Mbps)": "1/1",
+        "Avg Cost": "$1",
+        "DUration": "1 month",
+        "ISP": "-"
+    },
+    {
+        "City/Country": "Boise, ID",
+        "Avg Speed(Mbps)": "189/152",
+        "Avg Cost": "$65",
+        "DUration": "-",
+        "ISP": "CenturyLink"
+    },
+    {
+        "City/Country": "Boston, MA",
+        "Avg Speed(Mbps)": "270/27",
+        "Avg Cost": "$70",
+        "DUration": "-",
+        "ISP": "-"
+    },
+    {
+        "City/Country": "Vienna, Austria",
+        "Avg Speed(Mbps)": "40/10",
+        "Avg Cost": "$40",
+        "DUration": "-",
+        "ISP": "AKIS"
+    },
+    {
+        "City/Country": "England",
+        "Avg Speed(Mbps)": "15/4",
+        "Avg Cost": "£49.14 ($64.28)",
+        "DUration": "-",
+        "ISP": "TalkTalkBiz"
+    }
+]
+
+export { data };
+
---
+++ index.js
@@ -0,0 +1,65 @@
+import { Kerdx } from 'https://kade-95.github.io/kerdx/index.js';
+import { data } from './data.js';
+
+window.kerdx = new Kerdx();
+
+let appTitle = kerdx.createElement({
+    element: 'span', attributes: { id: 'app-title' }, children: [
+        { element: 'span', attributes: { class: 'app-title-dot', style: { backgroundColor: '#6FCF97' } } },
+        { element: 'span', attributes: { class: 'app-title-dot', style: { backgroundColor: '#F2994A' } } },
+        { element: 'a', attributes: { id: 'app-title-text' }, text: 'howmuchisdata?' }
+    ]
+});
+
+let searchArea = kerdx.createElement({
+    element: 'div', attributes: { id: 'search-area' }, children: [
+        { element: 'i', attributes: { class: 'fas fa-search' } },
+        { element: 'input', attributes: { id: 'search-box' } }
+    ]
+});
+
+let dataTable = kerdx.createTable({ title: 'Search result', contents: data })
+
+document.addEventListener('DOMContentLoaded', event => {
+    let main = document.body.makeElement({ element: 'main', attributes: { id: 'main-window' } });
+    main.makeElement([appTitle, searchArea, dataTable]);
+
+    search();
+});
+let firstCells = dataTable.find('.kerdx-table').find('.kerdx-table-column').findAll('.kerdx-table-column-cell');
+
+let rows = [];
+for (let i = 0; i < firstCells.length; i++) {
+    rows.push(firstCells[i].dataset.row);
+}
+
+function search() {
+    let searchBox = document.body.find('#search-box');
+    let tableRow;
+    searchBox.onChanged(value => {
+        for (let i = 0; i < rows.length; i++) {
+            let hide = false;
+            tableRow = dataTable.findAll(`.kerdx-table-column-cell[data-row="${i}"]`);
+
+            for (let j = 0; j < tableRow.length; j++) {
+                tableRow[j].cssRemove(['display']);
+            }
+
+            if (hide == false) {
+                hide = true;
+                for (let j = 0; j < tableRow.length; j++) {
+                    if (tableRow[j].textContent.toLowerCase().includes(value.toLowerCase())) {
+                        hide = false;
+                        break;
+                    }
+                }
+            }
+
+            if (hide) {
+                for (let j = 0; j < tableRow.length; j++) {
+                    tableRow[j].css({ display: 'none' });
+                }
+            }
+        }
+    });
+}
