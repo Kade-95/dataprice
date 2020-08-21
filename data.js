ce6d93456889510ba6b73d94e960e54c11a3c5be
kennedy.ikeka <kennedy.ikeka@ipigroupng.com>
2020-08-21 10:21:44 +0100

    First Commit

--- data.js
+++ data.js
@@ -1,168 +1,168 @@
 let data = [
     {
         "City/Country": "Dublin, Germany",
-        "Avg Speed(Mbps)": "300/-",
+        "Mbps Dn/Up": "300/-",
         "Avg Cost": "$158",
         "Duration": "-",
         "ISP": " "
     },
     {
         "City/Country": "Poznan, Poland",
-        "Avg Speed(Mbps)": "-/-",
+        "Mbps Dn/Up": "-/-",
         "Avg Cost": "100zł (~30USD)",
         "Duration": "-",
         "ISP": "-"
     },
     {
         "City/Country": "India",
-        "Avg Speed(Mbps)": "48/-",
+        "Mbps Dn/Up": "48/-",
         "Avg Cost": "966Rs",
         "Duration": "-",
         "ISP": "Hathway Broadband"
     },
     {
         "City/Country": "Berlin, Germany",
-        "Avg Speed(Mbps)": "100/-",
+        "Mbps Dn/Up": "100/-",
         "Avg Cost": "40€",
         "Duration": "-",
         "ISP": "Vodafone"
     },
     {
         "City/Country": "Berlin, Germany",
-        "Avg Speed(Mbps)": "200/-",
+        "Mbps Dn/Up": "200/-",
         "Avg Cost": "31€",
         "Duration": "-",
         "ISP": "Pyur"
     },
     {
         "City/Country": "Italy",
-        "Avg Speed(Mbps)": "500/200",
+        "Mbps Dn/Up": "500/200",
         "Avg Cost": "30€",
         "Duration": "-",
         "ISP": "-"
     },
     {
         "City/Country": "Portugal",
-        "Avg Speed(Mbps)": "400/100",
+        "Mbps Dn/Up": "400/100",
         "Avg Cost": "36€",
         "Duration": "-",
         "ISP": "-"
     },
     {
         "City/Country": "Slovenia",
-        "Avg Speed(Mbps)": "100/10",
+        "Mbps Dn/Up": "100/10",
         "Avg Cost": "24€",
         "Duration": "-",
         "ISP": "-"
     },
     {
         "City/Country": "Ireland",
-        "Avg Speed(Mbps)": "500/-",
+        "Mbps Dn/Up": "500/-",
         "Avg Cost": "60€",
         "Duration": "-",
         "ISP": "Virgin"
     },
     {
         "City/Country": "Budapest, Hungary",
-        "Avg Speed(Mbps)": "300/-",
+        "Mbps Dn/Up": "300/-",
         "Avg Cost": "$10",
         "Duration": "-",
         "ISP": "-"
     },
     {
         "City/Country": "Lusaka, Zambia",
-        "Avg Speed(Mbps)": "3/-",
+        "Mbps Dn/Up": "3/-",
         "Avg Cost": "ZMW 349(19)",
         "Duration": "-",
         "ISP": "Zamtel"
     },
     {
         "City/Country": "Morroco",
-        "Avg Speed(Mbps)": "4/-",
+        "Mbps Dn/Up": "4/-",
         "Avg Cost": "DHS 200 ($20)",
         "Duration": "-",
         "ISP": "IAM"
     },
     {
         "City/Country": "Hong Kong",
-        "Avg Speed(Mbps)": "1024/-",
+        "Mbps Dn/Up": "1024/-",
         "Avg Cost": "$20",
         "Duration": "-",
         "ISP": "-"
     },
     {
         "City/Country": "New York",
-        "Avg Speed(Mbps)": "210/11",
+        "Mbps Dn/Up": "210/11",
         "Avg Cost": "$70",
         "Duration": "-",
         "ISP": "Spectrum"
     },
     {
         "City/Country": "Nigeria",
-        "Avg Speed(Mbps)": "30/-",
+        "Mbps Dn/Up": "30/-",
         "Avg Cost": "$3",
         "Duration": "-",
         "ISP": "-"
     },
     {
         "City/Country": "Salt Lake City, Utah",
-        "Avg Speed(Mbps)": "780/890",
+        "Mbps Dn/Up": "780/890",
         "Avg Cost": "$65",
         "Duration": "1 month",
         "ISP": "-"
     },
     {
         "City/Country": "Netherlands",
-        "Avg Speed(Mbps)": "1024/-",
+        "Mbps Dn/Up": "1024/-",
         "Avg Cost": "$46",
         "Duration": "-",
         "ISP": "-"
     },
     {
         "City/Country": "Portugal",
-        "Avg Speed(Mbps)": "500/-",
+        "Mbps Dn/Up": "500/-",
         "Avg Cost": "35€ ($42)",
         "Duration": "-",
         "ISP": "-"
     },
     {
         "City/Country": "United Kingdom(UK)",
-        "Avg Speed(Mbps)": "1024/1024",
+        "Mbps Dn/Up": "1024/1024",
         "Avg Cost": "$40",
         "Duration": "-",
         "ISP": "-"
     },
     {
         "City/Country": "Venezuela",
-        "Avg Speed(Mbps)": "1/1",
+        "Mbps Dn/Up": "1/1",
         "Avg Cost": "$1",
         "Duration": "1 month",
         "ISP": "-"
     },
     {
         "City/Country": "Boise, ID",
-        "Avg Speed(Mbps)": "189/152",
+        "Mbps Dn/Up": "189/152",
         "Avg Cost": "$65",
         "Duration": "-",
         "ISP": "CenturyLink"
     },
     {
         "City/Country": "Boston, MA",
-        "Avg Speed(Mbps)": "270/27",
+        "Mbps Dn/Up": "270/27",
         "Avg Cost": "$70",
         "Duration": "-",
         "ISP": "-"
     },
     {
         "City/Country": "Vienna, Austria",
-        "Avg Speed(Mbps)": "40/10",
+        "Mbps Dn/Up": "40/10",
         "Avg Cost": "$40",
         "Duration": "-",
         "ISP": "AKIS"
     },
     {
         "City/Country": "England",
-        "Avg Speed(Mbps)": "15/4",
+        "Mbps Dn/Up": "15/4",
         "Avg Cost": "£49.14 ($64.28)",
         "Duration": "-",
         "ISP": "TalkTalkBiz"
